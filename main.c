5127e2512a2982600550c6e243ae10e2747c6629
gmarinelli <gmarinelli@localhost>
2017-04-24 11:51:48 -0300

    Serial y Vectorial funcionando

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+//autores: Bermudez Martin, Marinelli Giuliano
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,6 +29,12 @@
 })
 
 int main(int argc, char *argv[]) {
+    serial(argc, argv);
+    //vectorial(argc, argv);
+    return 0;
+}
+
+void serial(int argc, char *argv[]) {
     FILE *f;
     char *s, *res;
     int rows, cols, steps, n, i, j, k, p;
@@ -36,18 +43,6 @@ int main(int argc, char *argv[]) {
     char **mundo;
     char **mundoAux;
     char **aux;
-
-    /*se definen los vectores (registros) que se utilizaran para operar la matriz*/
-    __m128i vSup, vCen, vInf, vSumIni, vSumIniSftIzq, vSumIniSftDer, vSumTot,
-            vSumAdy, vCenEq0, vCenEq1, vAdyEq2, vAdyEq3, vAdyEq2OrEq3, vCenEq1AndAdyEq2OrEq3,
-            vCenEq0AndAdyEq3, vRes;
-    /*contienen 16 bytes con el numeros 0, 1, 2 y 3 cargados respectivamente*/
-    __m128i n0, n1, n2, n3;
-
-    n0 = _mm_set1_epi8(0);
-    n1 = _mm_set1_epi8(1);
-    n2 = _mm_set1_epi8(2);
-    n3 = _mm_set1_epi8(3);
 
     /*controla que se haya ingresado un argumento en la llamada*/
     if (argc != 2) {
@@ -71,35 +66,13 @@ int main(int argc, char *argv[]) {
 
     printf("cols %d\nrows %d\nsteps %d\n", cols, rows, steps);
 
-    /*aqui se asigna memoria alineada a las matrices mundo y mundoAux para luego
-     *  operar con mundo y guardar el resultado en mundoAux*/
-    /*el tamaño en memoria es equivalente a cada elemento de la matriz, ya que
-     se guardara continua en la memoria*/
-    /*if (posix_memalign((void*) &mundo, 16, (rows + 2)*(cols + 2)) != 0) {
-        perror("posix_memalign dio error para mundo");
-        exit(1);
-    }
-    if (posix_memalign((void*) &mundoAux, 16, (rows + 2)*(cols + 2)) != 0) {
-        perror("posix_memalign dio error para mundoAux");
-        exit(1);
-    }*/
-
-    posix_memalign((void*) &mundo, 16, (rows + 2) * sizeof (char *));
-    posix_memalign((void*) &mundoAux, 16, (rows + 2) * sizeof (char *));
-
-    for (i = 0; i < rows + 2; i++) {
-        posix_memalign((void*) &mundo[i], 16, (cols + 2 + (14 - (cols % 14)) + 10000) * sizeof (char));
-        posix_memalign((void*) &mundoAux[i], 16, (cols + 2 + (14 - (cols % 14)) + 10000) * sizeof (char));
-    }
-
-    /*asi se asignaba memoria cuando no estaba alineada*/
-    /*mundo = malloc((rows + 2) * sizeof (char *));
+    mundo = malloc((rows + 2) * sizeof (char *));
     mundoAux = malloc((rows + 2) * sizeof (char *));
 
     for (i = 0; i < rows + 2; i++) {
         mundo[i] = malloc((cols + 2) * sizeof (char));
         mundoAux[i] = malloc((cols + 2) * sizeof (char));
-    }*/
+    }
 
     /*inicializar elementos con 0 o 1 en matriz llamada "mundo"*/
     s = (char *) malloc((cols + 1) * sizeof (char));
@@ -110,12 +83,10 @@ int main(int argc, char *argv[]) {
     res = fgets(s, cols + 2, f);
     i = 1;
     printf("\n");
-
     while (res != NULL && i < rows + 1) {
-        // printf("Linea %i: %s => strlen %i (incluye salto de linea)\n", i, s, strlen(s));
-        for (j = 0; j < strlen(s) - 2; j++) {
+        //printf("Linea %i: %s => strlen %i (incluye salto de linea)\n", i, s, strlen(s));
+        for (j = 0; j < strlen(s) - 1; j++) {
             mundo[i][j + 1] = (s[j] == '.') ? 0 : 1;
-            //printf("ENTRO JOTA %i\n",j );
         }
 
         /* IMPORTANTE
@@ -124,7 +95,6 @@ int main(int argc, char *argv[]) {
 
         for (j = strlen(s) - 1; j < cols; j++) {
             mundo[i][j + 1] = 0;
-            //printf("22 ENTRO JOTA %i en I %i\n",j,i );
         }
         res = fgets(s, cols + 2, f);
         i++;
@@ -139,8 +109,9 @@ int main(int argc, char *argv[]) {
     short cantVivas = 0;
     //repetir cantidad de pasos
     do {
+
         /*filas y columnas de mas para hacer el mundo rendondo*/
-        //primera fila (por referencia, luego las esquinas se sobreescriben).
+        //primera fila (por referencia, luego las esquinas se sobreescrib       en).
         mundo[0] = mundo[rows];
         //ultima fila (por referencia, luego las esquinas se sobreescriben).
         mundo[rows + 1] = mundo[1];
@@ -158,9 +129,145 @@ int main(int argc, char *argv[]) {
             mundo[i][cols + 1] = mundo[i][1];
         }
 
-        printf("Iteracion %i mundo: \n", (iteracion));
+        printf("Iteracion %i mundo: \n", (iteracion + 1));
         IMPMUNDO(mundo);
 
+        //contar cantidad de celulas vecinas vivas para cada celula
+        for (i = 1; i < rows + 1; i++) {
+            for (j = 1; j < cols + 1; j++) {
+                cantVivas = mundo[i - 1][j - 1] + mundo[i - 1][j] + mundo[i - 1][j + 1] + mundo[i][j - 1] + mundo[i][j + 1] + mundo[i + 1][j - 1] + mundo[i + 1][j] + mundo[i + 1][j + 1];
+                if (mundo[i][j] == 1) {
+                    mundoAux[i][j] = (cantVivas == 2 || cantVivas == 3) ? 1 : 0;
+                } else {
+                    mundoAux[i][j] = (cantVivas == 3) ? 1 : 0;
+                }
+            }
+        }
+
+        aux = mundo;
+        mundo = mundoAux;
+        mundoAux = aux;
+        iteracion++;
+
+    } while (iteracion < steps);
+
+    printf("Estado final: \n");
+    IMPMUNDO(mundo);
+
+    fclose(f);
+    free(s);
+    free(mundo);
+    free(mundoAux);
+}
+
+void vectorial(int argc, char *argv[]) {
+    FILE *f;
+    char *s, *res;
+    int rows, cols, steps, n, i, j, k, p;
+
+    /*se definen los espacios de memoria donde se almacenaran las matrices*/
+    char **mundo;
+    char **mundoAux;
+    char **aux;
+
+    /*se definen los vectores (registros) que se utilizaran para operar la matriz*/
+    __m128i vSup, vCen, vInf, vSumIni, vSumIniSftIzq, vSumIniSftDer, vSumTot,
+            vAdyEq4, vAdyEq3, vRes;
+    /*contienen 16 bytes con el numeros 0, 1, 2 y 3 cargados respectivamente*/
+    __m128i n0, n1, n3, n4;
+
+    n0 = _mm_set1_epi8(0);
+    n1 = _mm_set1_epi8(1);
+    n3 = _mm_set1_epi8(3);
+    n4 = _mm_set1_epi8(4);
+
+    /*controla que se haya ingresado un argumento en la llamada*/
+    if (argc != 2) {
+        printf("Error: Debe indicar el nombre del archivo de entrada\nEj: $./main.o entrada.cells\n");
+        return 1;
+    }
+
+    /*lectura del encabezado del archivo que contiene el patron de celdas*/
+    f = fopen(argv[1], "r");
+    if (f == NULL) {
+        printf("Error al intentar abrir el archivo.\n");
+        return 1;
+    }
+
+    n = fscanf(f, "cols %d\nrows %d\nsteps %d\n", &cols, &rows, &steps);
+
+    if (n != 3) {
+        printf("Error: formato de archivo incorrecto\n");
+        return 1;
+    }
+
+    printf("cols %d\nrows %d\nsteps %d\n", cols, rows, steps);
+
+    posix_memalign((void*) &mundo, 16, (rows + 2) * sizeof (char *));
+    posix_memalign((void*) &mundoAux, 16, (rows + 2) * sizeof (char *));
+
+    for (i = 0; i < rows + 2; i++) {
+        posix_memalign((void*) &mundo[i], 16, (cols + 2 + (14 - (cols % 14))) * sizeof (char));
+        posix_memalign((void*) &mundoAux[i], 16, (cols + 2 + (14 - (cols % 14))) * sizeof (char));
+    }
+
+    /*inicializar elementos con 0 o 1 en matriz llamada "mundo"*/
+    s = (char *) malloc((cols + 1) * sizeof (char));
+    if (s == NULL) {
+        printf("Memoria llena.\n");
+        return 1;
+    }
+    res = fgets(s, cols + 2, f);
+    i = 1;
+    printf("\n");
+    while (res != NULL && i < rows + 1) {
+        //printf("Linea %i: %s => strlen %i (incluye salto de linea)\n", i, s, strlen(s));
+        for (j = 0; j < strlen(s) - 1; j++) {
+            mundo[i][j + 1] = (s[j] == '.') ? 0 : 1;
+        }
+
+        /* IMPORTANTE
+         para que funcione correctamente el archivo de entrada debe tener un 
+         salto de linea en la ultima fila para la matriz, dejando una linea en blanco.*/
+
+        for (j = strlen(s) - 1; j < cols; j++) {
+            mundo[i][j + 1] = 0;
+        }
+        res = fgets(s, cols + 2, f);
+        i++;
+    }
+
+    /*llenar con ceros las filas que falten*/
+    for (j = i; j < rows + 1; j++) {
+        memset(mundo[j], 0, cols + 2);
+    }
+
+    int iteracion = 0;
+    //repetir cantidad de pasos
+    do {
+
+        /*filas y columnas de mas para hacer el mundo rendondo*/
+        //primera fila (por referencia, luego las esquinas se sobreescriben).
+        mundo[0] = mundo[rows];
+        //ultima fila (por referencia, luego las esquinas se sobreescriben).
+        mundo[rows + 1] = mundo[1];
+        //esquina sup-izq
+        mundo[0][0] = mundo[rows][cols];
+        //esquina sup-der
+        mundo[0][cols + 1] = mundo[rows][1];
+        //esquina inf-izq
+        mundo[rows + 1][0] = mundo[1][cols];
+        //esquina inf-der
+        mundo[rows + 1][cols + 1] = mundo[1][1];
+        //primer y ultima columna (sin considerar las esquinas).
+
+        for (i = 1; i < rows + 1; i++) {
+            mundo[i][0] = mundo[i][cols];
+            mundo[i][cols + 1] = mundo[i][1];
+        }
+
+        printf("Iteracion %i mundo: \n", (iteracion));
+        IMPMUNDO(mundo);
 
         for (i = 0; i < rows; i++) {
             for (j = 0; j <= (cols / 14); j++) {
@@ -185,61 +292,33 @@ int main(int argc, char *argv[]) {
                 vSumTot = _mm_add_epi8(vSumIni, vSumIniSftIzq);
                 vSumTot = _mm_add_epi8(vSumTot, vSumIniSftDer);
 
-                /*resto la celula seleccionada para tener solo la suma de los
-                 adyacentes*/
-                vSumAdy = _mm_sub_epi8(vSumTot, vCen);
-
-                /*comparo para saber si la celula seleccionada esta viva*/
-                vCenEq1 = _mm_cmpeq_epi8(vCen, n1);
-
-                /*comparo para saber si la celula seleccionada esta muerta*/
-                vCenEq0 = _mm_cmpeq_epi8(vCen, n0);
-
-                /*comparo para saber si la suma de los adyacentes es igual a 2*/
-                vAdyEq2 = _mm_cmpeq_epi8(vSumAdy, n2);
                 /*comparo para saber si la suma de los adyacentes es igual a 3*/
-                vAdyEq3 = _mm_cmpeq_epi8(vSumAdy, n3);
+                vAdyEq3 = _mm_cmpeq_epi8(vSumTot, n3);
 
-                /*comparo para saber si la suma de los adyacentes es 2 o 3*/
-                vAdyEq2OrEq3 = _mm_or_si128(vAdyEq2, vAdyEq3);
+                /*como vAdyEq3 queda con FF en lugar de 1, pasar cada FF a un 1.*/
+                vAdyEq3 = _mm_and_si128(vAdyEq3, n1);
 
-                /*compara para saber si siendo que la celula seleccionada esta viva,
-                 la suma de adyacentes es 2 o 3*/
-                vCenEq1AndAdyEq2OrEq3 = _mm_and_si128(vCenEq1, vAdyEq2OrEq3);
+                /*hasta ahora tenemos vAdyEq3 con las celulas que tienen que 
+                 * estar vivas si tenian 3 adyacentes. (3 si estaba muerta o 2 si 
+                 * estaba viva) queda por verificar que si estaba viva siga 
+                 * estando viva con 4 adyacentes (3 mas ella misma).*/
+                vAdyEq4 = _mm_cmpeq_epi8(vSumTot, n4);
 
-                /*compara para saber si siendo que la celula seleccionada esta muerta,
-                 la suma de adyacentes es 3*/
-                vCenEq0AndAdyEq3 = _mm_and_si128(vCenEq0, vAdyEq3);
+                /*luego hacemos un and entre el vector de las celulas y vAdyEq4 
+                 * para que queden vivas las que estaban vivas.*/
+                vAdyEq4 = _mm_and_si128(vCen, vAdyEq4);
 
-                /*comparo para obtener finalmente si la celula queda viva o muerta*/
-                vRes = _mm_or_si128(vCenEq0AndAdyEq3, vCenEq1AndAdyEq2OrEq3);
+                /*unificamos ambos resultados vAdyEq3 y vAdyEq4.*/
+                vRes = _mm_or_si128(vAdyEq4, vAdyEq3);
 
-                /*el vRes quedara con un resultado booleano es decir con
-                 FF(un byte lleno de 1) en los datos true.
-                 Paso los FF a numeros 1.*/
-                vRes = _mm_and_si128(vRes, n1);
-
+                /*luego correr el resultado hacia la derecha para sobreescribir 
+                 * el resultado de la celda final de la iteracion anterior la 
+                 * cual no habia tenido en cuenta todas las vecinas*/
                 vRes = _mm_srli_si128(vRes, 1);
 
                 /*guardo en la matrizAux el nuevo estado de las 14 celulas analisadas*/
                 _mm_storeu_si128((__m128i*) (mundoAux[i + 1] + (j * 14) + 1), vRes);
 
-                /*esto es lo que se hacia antes*/
-                /*cantVivas = 0;
-                for (k = i - 1; k <= i + 1; k++) {
-                    for (p = j - 1; p <= j + 1; p++) {
-                        if (mundo[k][p] == 'O') {
-                            cantVivas++;
-                        }
-                    }
-                }
-                if (mundo[i][j] == 'O') {
-                    cantVivas--;
-                    mundoAux[i][j] = (cantVivas == 2 || cantVivas == 3) ? 'O' : '.';
-                } else {
-                    mundoAux[i][j] = (cantVivas == 3) ? 'O' : '.';
-                    }
-                 */
             }
         }
 
@@ -247,39 +326,14 @@ int main(int argc, char *argv[]) {
         mundo = mundoAux;
         mundoAux = aux;
         iteracion++;
+
     } while (iteracion < steps);
 
     printf("Estado final: \n");
     IMPMUNDO(mundo);
 
-    //    /*Salida por archivo de texto*/
-    //    FILE *fp;
-    //
-    //    //si no existe salida.txt lo creara
-    //    fp = fopen("salida.txt", "w+");
-    //    char buffer[cols];
-    //    fputs("cols ", fp);
-    //
-    //    /*cargo un buffer de char con el valor de cols, para despues copiar el buffer 
-    //    en el archivo de salida con fputs*/
-    //    sprintf(buffer, "%d", cols);
-    //    fputs(buffer, fp);
-    //    fputs("\n", fp);
-    //    fputs("rows ", fp);
-    //    sprintf(buffer, "%d", rows);
-    //    fputs(buffer, fp);
-    //    fputs("\n", fp);
-    //    fputs("steps ", fp);
-    //    sprintf(buffer, "%d", steps);
-    //    fputs(buffer, fp);
-    //    fputs("\n", fp);
-    //
-    //    for (i = 1; i < rows + 1; ++i) {
-    //        fwrite(*(mundo + i), sizeof (char), cols, fp);
-    //        fputs("\n", fp);
-    //    }
-    //    fclose(fp);
-
     fclose(f);
     free(s);
+    free(mundo);
+    free(mundoAux);
 }
