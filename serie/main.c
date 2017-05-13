be426e196405e14d66ddb9ffff441d6a752ecd5f
Martinnqn <bermudez_martin.nqn@hotmail.com>
2017-05-13 18:54:42 -0300

    serie optimizado. Ahora todas las sumas y restas ocurren en variables auxiliares, evitando que se repitan las mismas operaciones en cada itearcion.

--- serie/main.c
+++ serie/main.c
@@ -5,8 +5,8 @@
 
 /*macro que imprime la matriz del mundo*/
 #define IMPMUNDO(matriz) ({\
-    for (i = 1; i < rows + 1; ++i) {\
-        for (j = 1; j < cols + 1; ++j) {\
+    for (i = 1; i < rowsM1; ++i) {\
+        for (j = 1; j < colsM1; ++j) {\
             if (matriz[i][j] == 0) {\
                 printf(".");\
             } else {\
@@ -19,8 +19,8 @@
 
 /*macro que imprime la matriz completa*/
 #define IMPMUNDOCOMP(matriz) ({\
-    for (i = 0; i < rows + 2; i++) {\
-        for (j = 0; j < cols + 2; j++) {\
+    for (i = 0; i < rowsM2; i++) {\
+        for (j = 0; j < colsM2; j++) {\
             printf("%i", matriz[i][j]);\
         }\
         printf("\n");\
@@ -30,7 +30,7 @@
 int main(int argc, char *argv[]) {
     FILE *f;
     char *s, *res;
-    int rows, cols, steps, n, i, j, k, p;
+    int rows, cols, steps, n, i, j, k, p, colsM2, rowsM2, colsM1, rowsM1;
 
     /*se definen los espacios de memoria donde se almacenaran las matrices*/
     char **mundo;
@@ -58,26 +58,33 @@ int main(int argc, char *argv[]) {
     }
 
     printf("cols %d\nrows %d\nsteps %d\n", cols, rows, steps);
+    rowsM2 = rows + 2;
+    colsM2 = cols + 2;
 
-    mundo = malloc((rows + 2) * sizeof (char *));
-    mundoAux = malloc((rows + 2) * sizeof (char *));
+    rowsM1 = rows + 1;
+    colsM1 = cols + 1;
 
-    for (i = 0; i < rows + 2; i++) {
-        mundo[i] = malloc((cols + 2) * sizeof (char));
-        mundoAux[i] = malloc((cols + 2) * sizeof (char));
+    mundo = malloc((rowsM2) * sizeof (char *));
+    mundoAux = malloc((rowsM2) * sizeof (char *));
+
+    for (i = 0; i < rowsM2; i++) {
+        mundo[i] = malloc((colsM2) * sizeof (char));
+        mundoAux[i] = malloc((colsM2) * sizeof (char));
     }
 
     /*inicializar elementos con 0 o 1 en matriz llamada "mundo"*/
-    s = (char *) malloc((cols + 1) * sizeof (char));
+    s = (char *) malloc((colsM1) * sizeof (char));
     if (s == NULL) {
         perror("Memoria llena.\n");
         exit(1);
     }
-    res = fgets(s, cols + 2, f);
+    res = fgets(s, colsM2, f);
     i = 1;
-    while (res != NULL && i < rows + 1) {
+
+    int strlenAux = strlen(s) - 1;
+    while (res != NULL && i < rowsM1) {
         //printf("Linea %i: %s => strlen %i (incluye salto de linea)\n", i, s, strlen(s));
-        for (j = 0; j < strlen(s) - 1; j++) {
+        for (j = 0; j < strlenAux; j++) {
             mundo[i][j + 1] = (s[j] == '.') ? 0 : 1;
         }
 
@@ -85,18 +92,21 @@ int main(int argc, char *argv[]) {
          para que funcione correctamente el archivo de entrada debe tener un 
          salto de linea en la ultima fila para la matriz, dejando una linea en blanco.*/
 
-        for (j = strlen(s) - 1; j < cols; j++) {
+        for (j = strlenAux; j < cols; j++) {
             mundo[i][j + 1] = 0;
         }
-        res = fgets(s, cols + 2, f);
+        res = fgets(s, colsM2, f);
         i++;
     }
 
     /*llenar con ceros las filas que falten*/
-    for (j = i; j < rows + 1; j++) {
-        memset(mundo[j], 0, cols + 2);
+    for (j = i; j < rowsM1; j++) {
+        memset(mundo[j], 0, colsM2);
     }
-
+    int iAux1Men;
+    int iAux1Mas;
+    int jAux1Men;
+    int jAux1Mas;
     int iteracion = 0;
     short cantVivas = 0;
     //repetir cantidad de pasos
@@ -106,28 +116,32 @@ int main(int argc, char *argv[]) {
         //primera fila (por referencia, luego las esquinas se sobreescrib       en).
         mundo[0] = mundo[rows];
         //ultima fila (por referencia, luego las esquinas se sobreescriben).
-        mundo[rows + 1] = mundo[1];
+        mundo[rowsM1] = mundo[1];
         //esquina sup-izq
         mundo[0][0] = mundo[rows][cols];
         //esquina sup-der
-        mundo[0][cols + 1] = mundo[rows][1];
+        mundo[0][colsM1] = mundo[rows][1];
         //esquina inf-izq
-        mundo[rows + 1][0] = mundo[1][cols];
+        mundo[rowsM1][0] = mundo[1][cols];
         //esquina inf-der
-        mundo[rows + 1][cols + 1] = mundo[1][1];
+        mundo[rowsM1][colsM1] = mundo[1][1];
         //primer y ultima columna (sin considerar las esquinas).
-        for (i = 1; i < rows + 1; i++) {
+        for (i = 1; i < rowsM1; i++) {
             mundo[i][0] = mundo[i][cols];
-            mundo[i][cols + 1] = mundo[i][1];
+            mundo[i][colsM1] = mundo[i][1];
         }
 
         //printf("Iteracion %i mundo: \n", (iteracion + 1));
         //IMPMUNDO(mundo);
 
         //contar cantidad de celulas vecinas vivas para cada celula
-        for (i = 1; i < rows + 1; i++) {
-            for (j = 1; j < cols + 1; j++) {
-                cantVivas = mundo[i - 1][j - 1] + mundo[i - 1][j] + mundo[i - 1][j + 1] + mundo[i][j - 1] + mundo[i][j + 1] + mundo[i + 1][j - 1] + mundo[i + 1][j] + mundo[i + 1][j + 1];
+        for (i = 1; i < rowsM1; i++) {
+            iAux1Men=i-1;
+            iAux1Mas=i+1;
+            for (j = 1; j < colsM1; j++) {
+                jAux1Men=j-1;
+                jAux1Mas=j+1;
+                cantVivas = mundo[iAux1Men][jAux1Men] + mundo[iAux1Men][j] + mundo[iAux1Men][jAux1Mas] + mundo[i][jAux1Men] + mundo[i][jAux1Mas] + mundo[iAux1Mas][jAux1Men] + mundo[iAux1Mas][j] + mundo[iAux1Mas][jAux1Mas];
                 if (mundo[i][j] == 1) {
                     mundoAux[i][j] = (cantVivas == 2 || cantVivas == 3) ? 1 : 0;
                 } else {
@@ -149,6 +163,6 @@ int main(int argc, char *argv[]) {
     free(s);
     free(mundo);
     free(mundoAux);
-    
+
     return 0;
 }
